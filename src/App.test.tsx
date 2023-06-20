09ecb9ce396ca25fbcd98289da93fb2f5b54705d
mrajaona <maryse.rajaonary@gmail.com>
2023-06-20 15:03:19 +0200

    css-modules + sass

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -4,6 +4,6 @@ import App from "./App";
 
 test("renders learn react link", () => {
 	render(<App />);
-	const linkElement = screen.getByText(/learn react/i);
-	expect(linkElement).toBeInTheDocument();
+	const testText = screen.getByText(/potato/i);
+	expect(testText).toBeInTheDocument();
 });
