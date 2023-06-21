2f995325d820d0fb7fc72e2176fdc92cabdc5e51
mrajaona <maryse.rajaonary@gmail.com>
2023-06-21 12:53:45 +0200

    add gravity

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,32 @@
 import React from "react";
 import styles from "./App.module.scss";
+
 import { GameBox } from "./view/interface/GameBox";
 import { Player } from "./view/elements/Player";
+import { BIRD_SIZE, GAMEBOX_HEIGHT } from "./view/common/consts";
+
+const MIN_Y = 0;
+const MAX_Y = GAMEBOX_HEIGHT - BIRD_SIZE;
+const GRAVITY = 3;
+const TICK = 24; // ms
 
 function App() {
+	const [playerYPos, setPlayerYPos] = React.useState<number>(250);
+
+	React.useEffect(() => {
+		const i = setInterval(() => {
+			if (playerYPos > MIN_Y)
+				setPlayerYPos((playerYPos) => Math.max(MIN_Y, playerYPos - GRAVITY));
+		}, TICK);
+		return () => {
+			clearInterval(i);
+		};
+	});
+
 	return (
 		<div className={styles.wrapper}>
 			<GameBox>
-				<Player />
+				<Player bottom={playerYPos} />
 			</GameBox>
 		</div>
 	);
--- src/view/elements/Player.tsx
+++ src/view/elements/Player.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import styles from "./Player.module.scss";
 
-export const Player = () => {
-	return <div className={styles.bird}></div>;
+export const Player = ({ bottom }: { bottom: number }) => {
+	return <div className={styles.bird} style={{ bottom: bottom + "px" }}></div>;
 };
