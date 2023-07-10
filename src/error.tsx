9b133fe91dfc2faa65614a4b4a7e4f6bad86da59
mrajaona <maryse.rajaonary@gmail.com>
2023-07-10 11:59:16 +0200

    error page is a page

--- src/error.tsx
+++ src/error.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { isRouteErrorResponse, useRouteError } from "react-router-dom";
+import Page from "./page/Page";
 
 export default function ErrorPage() {
 	const error = useRouteError();
@@ -10,12 +11,14 @@ export default function ErrorPage() {
 		: (error as { message?: string }).message;
 
 	return (
-		<div id="error-page">
-			<h1>Oops!</h1>
-			<p>Sorry, an unexpected error has occurred.</p>
-			<p>
-				<i>{message}</i>
-			</p>
-		</div>
+		<Page>
+			<div id="error-page">
+				<h1>Oops!</h1>
+				<p>Sorry, an unexpected error has occurred.</p>
+				<p>
+					<i>{message}</i>
+				</p>
+			</div>
+		</Page>
 	);
 }
